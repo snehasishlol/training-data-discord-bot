f694e5a8ab431cec24ab9d819e465afd945c2de6
Snehasish Ray <snehasishcodes@gmail.com>
2023-03-18 00:26:45 +0530

    The Generate Command | Added Comments

--- commands/generate.js
+++ commands/generate.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs from 'fs'; // we will use the NodeJS FS or File system module. We don't need to install this if you have NodeJS installed.
 
 export default {
     exec: async (message) => {
@@ -13,14 +13,14 @@ export default {
       `
         })
 
-        const filter = m => m.author.bot == false;
+        const filter = m => m.author.bot == false; // Filter out the bots
 
         const inputCollector = message.channel.createMessageCollector({
             max: 1,
             filter,
             time: 600000,
             errors: ['time']
-        });
+        }); // Valid for 600k ms
 
         inputCollector.on('collect', async (input) => {
             message.channel.send(`
@@ -56,13 +56,13 @@ export default {
                     if (err) {
                         console.log(err);
                     } else {
-                        let obj = JSON.parse(data);
+                        let obj = JSON.parse(data); // parses the file
                         obj.data.push({
                             input: `${input.content}`,
                             output: `${output.content}`
                         });
-                        let json = JSON.stringify(obj, null, 4);
-                        fs.writeFile('data/training.json', json, 'utf8', () => message.channel.send('Done.'));
+                        let json = JSON.stringify(obj, null, 4); // converts to JSON, with `4` as the indent for formatting and multiple lines
+                        fs.writeFile('data/training.json', json, 'utf8', () => message.channel.send('Done.')); // writes or edits the file
                         message.channel.send('File saved.')
                     }
                 });
